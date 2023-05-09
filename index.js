65ef9c02d97c8b7cc957218f3f56bccfd96737c0
mishnan <mishnanc@gmail.com>
2023-05-09 14:49:27 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 mongoose.set('strictQuery',false)
-mongoose.connect('mongodb://127.0.0.1:27017/user_management_system');
+require('dotenv').config()
+mongoose.connect(process.env.mongodb);
 const path=require("path")
 const nocache = require('nocache')
 const express=require('express');  
